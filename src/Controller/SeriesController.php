371a8341cdfe36a736ce49c4a21b68c6825463ca
João Gabriel Cardoso <joaogabrielrcardoso@gmail.com>
2023-05-17 18:54:02 -0300

    Add create series route function

--- src/Controller/SeriesController.php
+++ src/Controller/SeriesController.php
@@ -2,16 +2,25 @@
 
 namespace App\Controller;
 
+use App\Entity\Series;
+use App\Repository\SeriesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SeriesController extends AbstractController
 {
+
+    public function __construct(private readonly SeriesRepository $seriesRepository)
+    {
+    }
+
     #[Route('/series', name: 'app_series', methods: ['GET'])]
     public function index(): Response
     {
-        $series = ['A', 'B'];
+        $series = $this->seriesRepository->findAll();
 
         return $this->render('series/index.html.twig', [
             'series' => $series,
@@ -22,5 +31,16 @@ class SeriesController extends AbstractController
     public function formCreate(): Response
     {
         return $this->render('series/form/create.html.twig');
+    }
+
+    #[Route('/series', name: 'app_series_create', methods: ['POST'])]
+    public function create(Request $request): Response
+    {
+        $inputSeriesName = $request->request->get('name');
+        $series = new Series($inputSeriesName);
+
+        $this->seriesRepository->save(entity: $series, flush: true);
+
+        return new RedirectResponse('/series');
     }
 }
